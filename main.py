27008622ba34c5f5f628d03d6ca244d07ae896f3
Itay Tohar <itay.tohar@gmail.com>
2022-11-08 21:10:47 +0200

    addCorsSupport

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask, request, jsonify
 from TadoneServices.controller.tadone_controller import tadone_controller
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(tadone_controller)
 
 
@@ -21,4 +22,4 @@ def main_app_entry():
 # TODO - Use Python environments so this is only run in development time.
 # Unmark in case of local development. Remark in case of production deployment.
 # app.run(ssl_context=("certificates/cert.pem", "certificates/key.pem"))
-app.run(port=int(os.environ.get("PORT", 5005)), host='0.0.0.0', debug=True)
+app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0', debug=True)
